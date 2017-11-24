a7671039f6a31821d134d58c8439e3aadefbb199
9king <871430453@qq.com>
2017-11-24 09:27:04 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/dangjian_first/application/BaseApplication.java
@@ -0,0 +1,18 @@
+package com.mycompany.dangjian_first.application;
+
+import android.app.Application;
+
+/**
+ * 项目名： Dangjian_First
+ * 包名：com.mycompany.dangjian_first.application
+ * 文件名:BaseApplication
+ * 创建者：zhuli
+ * 创建时间: 2017/11/24
+ * 描述:  Application的基类
+ */
+public class BaseApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
